a2f170bc8591bb521a2384f86edbf5c93d678932
Thiago Ferreira <thiagodnf@gmail.com>
2022-07-29 09:06:46 -0400

    udpated boostrap and jquery

--- js/index.js
+++ js/index.js
@@ -6,86 +6,86 @@ function getRandomInteger(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function disableInput(disable=true){
+function disableInput(disable = true) {
     $("#form-number #number").prop("disabled", disable);
     $("#form-number button[type='submit']").prop("disabled", disable);
     $("#btn-give-up").prop("disabled", disable);
 }
 
-function updatePlaceHolder(min, max){
-    if(showHint){
-        $("#number").attr("placeholder", "Type the number between "+min+" and "+max);
+function updatePlaceHolder(min, max) {
+    if (showHint) {
+        $("#number").attr("placeholder", "Type the number between " + min + " and " + max);
         $("#number").attr("min", min);
         $("#number").attr("max", max);
-    }else{
+    } else {
         $("#number").attr("placeholder", "Type the number between 1 and 1000");
         $("#number").attr("min", 1);
         $("#number").attr("max", 1000);
     }
 }
 
-$(function(){
+$(function () {
 
-    var guessNumber = getRandomInteger(1,1000);
-    var minNumber  = 1;
-    var maxNumber  = 1000;
+    var guessNumber = getRandomInteger(1, 1000);
+    var minNumber = 1;
+    var maxNumber = 1000;
 
     var $result = $(".result");
 
     var attempts = 1;
 
-    $("#btn-give-up").click(function(event){
+    $("#btn-give-up").click(function (event) {
 
         event.preventDefault();
 
-        if(confirm("Are you sure?")){
+        if (confirm("Are you sure?")) {
             disableInput(true);
-            $result.html("The number I was thinking of was "+guessNumber);
-            $result.addClass( "alert alert-warning" );
+            $result.html("The number I was thinking of was " + guessNumber);
+            $result.addClass("alert alert-warning");
         }
 
         return false;
     });
 
-    $("#btn-show-hint").click(function(){
+    $("#btn-show-hint").click(function () {
         showHint = $(this).is(":checked");
         updatePlaceHolder(minNumber, maxNumber);
     });
 
-    $("#form-number").submit(function(event){
+    $("#form-number").submit(function (event) {
 
         event.preventDefault();
 
         var $number = $(this).find("#number");
 
-        var number = $number.val().trim() ;
+        var number = $number.val().trim();
 
-        if(!number){
+        if (!number) {
             return;
         }
 
         disableInput(true);
 
-        if(number == guessNumber){
-            $result.html("Good job! You guessed my number in "+attempts+" guesses!");
-            $result.addClass( "alert alert-success" );
-        }else{
+        if (number == guessNumber) {
+            $result.html("Good job! You guessed my number in " + attempts + " guesses!");
+            $result.addClass("alert alert-success");
+        } else {
             var text = "";
 
-            if(number < guessNumber){
+            if (number < guessNumber) {
                 text = "Your guess is too low";
                 minNumber = number;
-            }else if(number > guessNumber){
+            } else if (number > guessNumber) {
                 text = "Your guess is too high";
                 maxNumber = number;
             }
 
             $result.html(text);
-            $result.parent().addClass( "alert alert-danger" );
+            $result.parent().addClass("alert alert-danger");
 
-            $result.effect( "bounce", {}, 1200, function(){
+            $result.effect("bounce", {}, 1200, function () {
                 $result.text("Take a Guess!");
-                $result.parent().removeClass( "alert alert-danger" );
+                $result.parent().removeClass("alert alert-danger");
                 $number.val("");
                 disableInput(false);
                 $number.focus();
